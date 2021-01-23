aa124dad4487ced88cc0d8b1f708736d5ddd5284
Josh <josh.hackney13@gmail.com>
2021-01-23 08:08:51 -0500

    door opens at game start

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FRotator CurrentRotation = GetOwner()->GetActorRotation();
+	ClosedDoor = CurrentRotation.Yaw;
+	OpenDoor = ClosedDoor - 90.f;
+	CurrentYaw = ClosedDoor;
+	HalfOpenDoor = ClosedDoor - 45.f;
+		
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+
+	// OpenDoor.Yaw = FMath::FInterpTo(ClosedDoor, TargetYaw, DeltaTime, 2);
+	CurrentYaw = FMath::Lerp(CurrentYaw, OpenDoor, 0.01f);
+	FRotator DoorRotation = GetOwner()->GetActorRotation();
+	DoorRotation.Yaw = CurrentYaw;
+
+	GetOwner()->SetActorRotation(DoorRotation);
+
+	
+}
+
